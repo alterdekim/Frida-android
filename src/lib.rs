4c9f9c85a010192d989a143ea26abece567a8823
alterdekim <alterwain@protonmail.com>
2024-09-15 21:44:01 +0300

    Changes to be committed: 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
 }
 
 
+/// Expose the JNI interface for android below
 #[cfg(target_os="android")]
 #[allow(non_snake_case)]
 pub mod android {
@@ -24,9 +25,9 @@ pub mod android {
     use self::jni::sys::{jstring};
 
     #[no_mangle]
-    pub unsafe extern fn Java_com_alterdekim_frida_FridaVPN_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
+    pub unsafe extern fn Java_com_mozilla_greetings_RustGreetings_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
         // Our Java companion code might pass-in "world" as a string, hence the name.
-        let world = rust_greeting(env.get_string(&java_pattern).expect("invalid pattern string").as_ptr());
+        let world = rust_greeting(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
         // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
         let world_ptr = CString::from_raw(world);
         let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
