f467fa77ca2a21392352fff6b9ebb2b402df66f2
alterdekim <alterwain@protonmail.com>
2024-09-15 18:32:11 +0300

    Changes to be committed: 	new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(target_os="android")]
+#[allow(non_snake_case)]
+pub mod android {
+	extern crate jni;
+	use super::*;
+	use self::jni::JNIEnv;
+	use self::jni::objects::{JClass, JString};
+	use self::jni::sys::{jstring};#[no_mangle]
+	pub unsafe extern fn Java_com_example_greet_Hello_greeting(env: JNIEnv, _: JClass, java_name: JString) -> jstring {
+
+		let name: String = env.get_string(java_pattern).expect("invalid pattern string").unwrap().into();
+		let mut greeting_string: String = "Hello ".to_owned();
+
+		greeting_string.push_str(&name);env.new_string(greeting_string).unwrap().into_inner()
+	}
+}
