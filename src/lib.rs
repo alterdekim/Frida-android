c82d8dbe7ada9ff5a469ee82b0175c8d099622d2
alterdekim <alterwain@protonmail.com>
2024-09-15 19:12:06 +0300

    Changes to be committed: 	modified:   Cargo.toml 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod android {
 	use self::jni::JNIEnv;
 	use self::jni::objects::{JClass, JString};
 	use self::jni::sys::{jstring};#[no_mangle]
-	pub unsafe extern fn Java_com_example_greet_Hello_greeting(env: JNIEnv, _: JClass, java_name: JString) -> jstring {
+	pub unsafe extern fn Java_com_alterdekim_frida_VPN_greeting(env: JNIEnv, _: JClass, java_name: JString) -> jstring {
 
 		let name: String = env.get_string(java_pattern).expect("invalid pattern string").unwrap().into();
 		let mut greeting_string: String = "Hello ".to_owned();
