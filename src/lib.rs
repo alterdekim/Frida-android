4ef80aafee6a7e1dd88f50cc5873c552f7b9ab31
alterdekim <alterwain@protonmail.com>
2024-09-20 03:24:29 +0300

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ pub mod android {
             Ok(string) => string,
         };
 
-        let config: ClientConfiguration = serde_yaml::from_slice(RFC4648.decode(cfg_raw.as_bytes()).unwrap()).expect("Bad client config file structure");
+        let config: ClientConfiguration = serde_yaml::from_slice(RFC4648.decode(cfg_raw.as_bytes()).unwrap().as_slice()).expect("Bad client config file structure");
         //client::client_mode(config);
 
         //let output = env.new_string("gabber").expect("Couldn't create java string!");
